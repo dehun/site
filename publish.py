9de4241a5364ddc01f060558d7b266d83c459164
Yuriy Netesov <retnuhed@gmail.com>
2014-07-01 11:04:00 +0300

    update

--- publish.py
+++ publish.py
@@ -1,4 +1,4 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_call('rsync -rva --progress ./static/* ../dehun', shell=True)
+    subprocess.check_call('rsync -rva --progress ./static/* ../dehun.bitbucket.org', shell=True)
