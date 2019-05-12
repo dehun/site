e3c1b099021c0f599c8175f91e20673aa250f3c7
Yuriy Netesov <yuriy.netesov@gmail.com>
2019-05-12 13:31:37 +0200

    fix

--- publish.py
+++ publish.py
@@ -1,4 +1,4 @@
 import subprocess
 
 if __name__ == '__main__':
-    subprocess.check_call('rsync -rva --exclude ".git" --progress ./static/* ../dehun.gitlab.io', shell=True)
+    subprocess.check_call('rsync -rva --no-links --exclude ".git" --progress ./static/* ../dehun.gitlab.io/public', shell=True)
